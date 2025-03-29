0ebdbe91a650cacf1e089336ab44919d91e9a31b
slowix <qudeglas@gmail.com>
2025-03-29 14:20:18 +0100

    Ajout du 2eme exo : Gérer l'orientation du personnage

--- Exo_1_Deplacer_personnage.cs
+++ Exo_1_Deplacer_personnage.cs
@@ -5,17 +5,28 @@ public class Player_Controller : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
-        // Déplacement horizontal
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveHorizontal * speed, rb2d.linearVelocity.y);
         rb2d.linearVelocity = movement;
+
+
+        if (moveHorizontal > 0)
+        {
+            spriteRenderer.flipX = false; 
+        }
+        else if (moveHorizontal < 0)
+        {
+            spriteRenderer.flipX = true; 
+        }
     }
 }
